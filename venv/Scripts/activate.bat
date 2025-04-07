31731c45fe749df08e50fef4d296d2f611a8bc89
Matheus <mthfduarte@gmail.com>
2025-04-07 18:31:18 -0300

    Atualiza README.md e adiciona arquivos de configuração

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Desktop\Aurora\venv
+set VIRTUAL_ENV=C:\Users\User\Desktop\Aurora\Sistema-de-vendas\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
